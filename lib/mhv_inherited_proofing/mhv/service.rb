15836b1a1a23299b8cdba6f41cc21161ea9cfde0
Josh Scanish <josh.scanish@oddball.io>
2022-03-21 16:15:16 -0400

    Find correlation_id

--- lib/mhv_inherited_proofing/mhv/service.rb
+++ lib/mhv_inherited_proofing/mhv/service.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+require 'mhv_inherited_proofing/mhv/configuration'
+
 module MhvInheritedProofing::Mhv
   class Service < Common::Client::Base
+    configuration MhvInheritedProofing::Mhv::Configuration
+
+    def correlation_id
+      response = perform(
+        :get, "#{config.valid_id_url}/#{ICN}", nil, { 'appToken' => config.app_token, 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+      )
+      response['correlationId']
+    rescue e
+      raise e
+    end
   end
 end
