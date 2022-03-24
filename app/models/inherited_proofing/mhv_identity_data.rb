182323d019d188840428bf4256c8d10cafc902ee
Josh Scanish <josh.scanish@oddball.io>
2022-03-24 16:48:03 -0400

    Add user_uuid, change ttl

--- app/models/inherited_proofing/mhv_identity_data.rb
+++ app/models/inherited_proofing/mhv_identity_data.rb
@@ -6,9 +6,10 @@ module InheritedProofing
     redis_ttl REDIS_CONFIG[:mhv_identity_data][:each_ttl]
     redis_key :code
 
+    attribute :user_uuid, String
     attribute :code, String
     attribute :data, Hash
 
-    validates(:code, :data, presence: true)
+    validates(:user_uuid, :code, :data, presence: true)
   end
 end
