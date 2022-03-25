1ea4d4466b78335be713aea6778c9b6e657c6090
John Bramley <bramleyjl@gmail.com>
2022-03-25 09:51:38 -0600

    syntax fixes

--- app/controllers/inherited_proofing_controller.rb
+++ app/controllers/inherited_proofing_controller.rb
@@ -1,39 +1,37 @@
-
 # frozen_string_literal: true
 
 require 'inherited_proofing/mhv/service'
 require 'inherited_proofing/logingov/service'
 
 class InheritedProofingController < ApplicationController
+  def auth
+    load_user
+    return unless @current_user
 
-    def auth
-      load_user 
-      return unless @current_user
+    mhv_data = mhv_inherited_proofing_service(@current_user).identity_proof_data
+    store_mhv_data(mhv_data)
+    render body: logingov_inherited_proofing_service.render_auth(auth_code: mhv_data[:code]),
+           content_type: 'text/html'
+  rescue => e
+    render json: { errors: e }, status: :bad_request
+  end
 
-      mhv_data = mhv_inherited_proofing_service(@current_user).identity_proof_data
-      store_mhv_data(mhv_data)
-      render body: logingov_inherited_proofing_service.render_auth(auth_code: mhv_data[:code]),
-             content_type: 'text/html'
-    rescue => e
-      render json: { errors: e }, status: :bad_request
-    end
+  private
 
-    private
+  def store_mhv_data(data)
+    inherited_proofing_mhv_data = InheritedProofing::MHVIdentityData.new(
+      code: data[:code],
+      user_uuid: @current_user.uuid,
+      data: data
+    )
+    inherited_proofing_mhv_data.save! if inherited_proofing_mhv_data.valid?
+  end
 
-    def store_mhv_data(data)
-      inherited_proofing_mhv_data = InheritedProofing::MHVIdentityData.new(
-        code: data[:code],
-        user_uuid: @current_user.uuid,
-        data: data
-      )
-      inherited_proofing_mhv_data.save! unless !inherited_proofing_mhv_data.valid?
-    end
+  def mhv_inherited_proofing_service(user = nil)
+    @mhv_inherited_proofing_service ||= InheritedProofing::MHV::Service.new(user)
+  end
 
-    def mhv_inherited_proofing_service(user = nil)
-      @mhv_inherited_proofing_service ||= InheritedProofing::MHV::Service.new(user)
-    end
-
-    def logingov_inherited_proofing_service
-      @logingov_inherited_proofing_service ||= InheritedProofing::Logingov::Service.new
-    end
+  def logingov_inherited_proofing_service
+    @logingov_inherited_proofing_service ||= InheritedProofing::Logingov::Service.new
+  end
 end
