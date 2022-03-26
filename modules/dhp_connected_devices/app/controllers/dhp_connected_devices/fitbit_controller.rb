8af1e30c6c51d3c71452d0176b0f64ac0af3336c
Yoni Doron-Peters <jonathan.d@thoughtworks.com>
2022-03-25 21:30:33 -0600

    DHP-92 Refactor code to 1. Generate PKCE verifier and challenge in an initializer 2. Extract Fitbit properties to settings 3. Extract client Faraday configuration

--- modules/dhp_connected_devices/app/controllers/dhp_connected_devices/fitbit_controller.rb
+++ modules/dhp_connected_devices/app/controllers/dhp_connected_devices/fitbit_controller.rb
@@ -1,80 +1,28 @@
+# frozen_string_literal: true
+
 module DhpConnectedDevices
   class FitbitController < ApplicationController
     skip_before_action :authenticate
 
-    def initialize
-      super
-      # TODO move to config/initializers
-      @client_id = '<client_id>'
-      @client_secret = '<client_secret>'
-      @client = FitbitAPI::Client.new(client_id: @client_id,
-                                      client_secret: @client_secret,
-                                      redirect_uri: 'http://localhost:3000/dhp_connected_devices/fitbit-callback',
-                                      scope: 'heartrate activity nutrition sleep')
-      # TODO use pkce generator library
-      # @pkce_challenge = PkceChallenge.challenge(char_length: 128)
-      @code_verifier = SecureRandom.alphanumeric(128)
-      @code_challenge = generate_pkce_challenge(@code_verifier)
-      @connection = connection
-    end
-
-    def index
-      render json: { greeting: "hello" }
-    end
-
     def connect
-      auth_url = @client.auth_url + "&code_challenge=#{@code_challenge}&code_challenge_method=S256"
+      auth_url = fitbit_api.auth_url
       redirect_to auth_url
     end
 
     def callback
       auth_code = callback_params[:code]
-      # token = @client.get_token(auth_code)
-      res = post('https://api.fitbit.com/oauth2/token',
-                 "client_id=#{@client_id}&code=#{auth_code}&code_verifier=#{@code_verifier}&grant_type=authorization_code&redirect_uri=http://localhost:3000/dhp_connected_devices/fitbit-callback")
-      p(res)
+      res = fitbit_api.get_token(auth_code)
       render json: { code: callback_params[:code], token: res.body }
     end
 
     private
 
-    def post(path, params)
-      @connection.post(path) { |req| req.body = params }
-    end
-
-    def connection
-      Faraday.new(url: url, headers: headers) do |conn|
-        # conn.response :health_quest_errors
-        # conn.use :health_quest_logging
-        conn.adapter Faraday.default_adapter
-      end
-    end
-
-    def url
-      'https://api.fitbit.com/oauth2/token'
-    end
-
-    def headers
-      {
-        'Authorization' => "Basic #{basicAuth}",
-        'Content-Type' => 'application/x-www-form-urlencoded'
-      }
-    end
-
-    def basicAuth
-      Base64.urlsafe_encode64("#{@client_id}:#{@client_secret}")
+    def fitbit_api
+      @fitbit_client ||= DhpConnectedDevices::Fitbit::Client.new
     end
 
     def callback_params
       params.permit(:code)
-    end
-
-    def generate_pkce_challenge(code_verifier)
-      urlsafe_base64(Digest::SHA256.base64digest(code_verifier))
-    end
-
-    def urlsafe_base64(base64_str)
-      base64_str.tr("+/", "-_").tr("=", "")
     end
   end
 end
