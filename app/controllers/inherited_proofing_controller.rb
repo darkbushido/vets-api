295b10f2b6d555f24bfc1ae2d95cc7a4bd37383d
John Bramley <bramleyjl@gmail.com>
2022-03-29 17:31:09 -0600

    merge conflict fixes

--- app/controllers/inherited_proofing_controller.rb
+++ app/controllers/inherited_proofing_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'inherited_proofing/mhv/service'
+require 'inherited_proofing/mhv/inherited_proofing/verifier'
 require 'inherited_proofing/logingov/service'
 
 class InheritedProofingController < ApplicationController
@@ -8,30 +8,17 @@ class InheritedProofingController < ApplicationController
     load_user
     return unless @current_user
 
-    mhv_data = mhv_inherited_proofing_service(@current_user).identity_proof_data
-    return unless mhv_data['code'].exists?
+    inherited_proofing_verifier = InheritedProofing::MHV::InheritedProofingVerifier.new(@current_user)
+    auth_code = inherited_proofing_verifier.perform
+    return unless auth_code.exists?
 
-    store_mhv_data(mhv_data)
-    render body: logingov_inherited_proofing_service.render_auth(auth_code: mhv_data[:code]),
+    render body: logingov_inherited_proofing_service.render_auth(auth_code: auth_code),
            content_type: 'text/html'
   rescue => e
     render json: { errors: e }, status: :bad_request
   end
 
   private
-
-  def store_mhv_data(data)
-    inherited_proofing_mhv_data = InheritedProofing::MHVIdentityData.new(
-      code: data[:code],
-      user_uuid: @current_user.uuid,
-      data: data
-    )
-    inherited_proofing_mhv_data.save! if inherited_proofing_mhv_data.valid?
-  end
-
-  def mhv_inherited_proofing_service(user = nil)
-    @mhv_inherited_proofing_service ||= InheritedProofing::MHV::Service.new(user)
-  end
 
   def logingov_inherited_proofing_service
     @logingov_inherited_proofing_service ||= InheritedProofing::Logingov::Service.new
